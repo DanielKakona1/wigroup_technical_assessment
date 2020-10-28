24cbcf86fcc953dfc3f09fe1945ffde078b92c46
Danielkakona <Daniel@bridgelabs.design>
2020-10-28 12:40:49 +0200

    Initial setup

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppContainer from './Routes'
+
+
+export default function App() {
+
+  return (
+    <AppContainer/> 
+
+  );
+}
+
+
---
+++ Routes.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as Screens from './src/screens';
+import { navigationRef, Routes } from './src/navigation/NavigationServices';
+;
+
+const Stack = createStackNavigator();
+
+const AppContainer = () => {
+  return (
+    <NavigationContainer ref={navigationRef}>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name={Routes.Home} component={Screens.Home} />
+        <Stack.Screen name={Routes.AutoSearch} component={Screens.AutoSearch} />
+        <Stack.Screen name={Routes.Wikipedia} component={Screens.Wikipedia} />
+        <Stack.Screen name={Routes.WikipediaDetails} component={Screens.WikipediaDetails} />
+        <Stack.Screen name={Routes.AsyncStorage} component={Screens.AsyncStorage} />
+        <Stack.Screen name={Routes.Welcome} component={Screens.Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppContainer;
